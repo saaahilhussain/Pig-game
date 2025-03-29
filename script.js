8d88221d1d1371afeb6fa8c9c18c0033ace4684c
Sahil <140058338+saaahilhussain@users.noreply.github.com.>
2025-03-30 02:48:29 +0530

    initial-values

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const dice = document.querySelector('.dice');
+
+//initial-values
+dice.classList.add('hidden');
+score0El.textContent = 0;
+score1El.textContent = 0;
